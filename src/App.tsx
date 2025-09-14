239f85dcdaa32f12d2c2d3bf246c16a78cf6c4b2
SashaDyka <beli.rothi2005@gmail.com>
2025-09-14 12:34:51 +0300

    write basic structure App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
 import { useState } from 'react'
+import type { Bill, Person } from './types/types'
+import BillList from './components/BillList';
+import BillEditor from './components/BillEditor';
 import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bills, setBills] = useState<Bill[]>([]);
+  const [selectBill, setSelectBill] = useState<Bill | null>(null);
+
+  const createBill = () => {
+    const tempBill: Bill = {
+      id: 'temp-123',
+      totalAmount: 8,
+      tipPercent: 5,
+      peopleCount: 1,
+      people: [],
+    };
+    
+    setSelectBill(tempBill);
+  };
+  
+  const closeBill = () => {
+    setSelectBill(null);
+  }
+  
+  
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div>
+      <h1>Tie counter</h1>
+      
+      {selectBill? (
+        <BillEditor bill={selectBill} />
+      ) : (
+        <BillList bills ={bills} onSelectBill = {setSelectBill} />
+      )}
+      
+      <button onClick={createBill}>Create Bill</button>
+      <button onClick={closeBill}>Close</button>
+
+    </div>
+      
   )
 }
 
