c47a22a683ec3a312928098f0ce6f84ba3d60620
SashaDyka <beli.rothi2005@gmail.com>
2025-09-14 12:36:45 +0300

    write basic bill editor structure

--- src/components/BillEditor.tsx
+++ src/components/BillEditor.tsx
@@ -1,2 +1,16 @@
-const BillEditor = () => <div>Iput for edit bill</div>;
+import React from 'react';
+import type { Bill } from '../types/types';
+
+interface BillEditorProps {
+  bill: Bill;
+} 
+
+const BillEditor: React.FC<BillEditorProps> = ({ bill }) => {
+  return (
+    <div>
+      <h2>Edit bill: {bill.id}</h2>
+    </div>
+  );
+};
+
 export default BillEditor;
