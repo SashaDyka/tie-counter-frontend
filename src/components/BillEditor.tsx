d0106ba6c0b5b4af1ab480c076b6d3d564ef87fe
SashaDyka <beli.rothi2005@gmail.com>
2025-09-14 13:35:08 +0300

    example: base view of a component

--- src/components/BillEditor.tsx
+++ src/components/BillEditor.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import { useState } from 'react'
 import type { Bill } from '../types/types';
 
 interface BillEditorProps {
-  bill: Bill;
+  bill: Bill; // 1 props: приходящие данные 
+  onSave: (updatedBill: Bill) => void; //функция, которую App передает TaskEditor нажата btn "Сохранить" 
+  onCancel: () => void;
 } 
 
 const BillEditor: React.FC<BillEditorProps> = ({ bill }) => {
+    //2 state: нужно хранить значения, которые пользователь вводит в поля формы
+    const [billAmount, setBillAmount] = useState(bill.totalAmount);
+    const [tipPercent, setTipPercent] = useState(bill.tipPercent);
+    const [peopleCount, setpeopleCount] = useState(bill.peopleCount);
+
+
+    //4 функции-обработчики
+    const handleCancel = () => {
+        onCancel();
+    }
+    
+    //3: разметка
   return (
     <div>
-      <h2>Edit bill: {bill.id}</h2>
+      <h2>Bill #1</h2>
+      <input
+        type="text"
+        value={billAmount}
+        onChange={(e) => setBillAmount(Number(e.target.value))}/>
+
     </div>
+    
+    
+
+   
   );
 };
 
