234e67ee4160c1b817af7484732e87bfccd23cbf
SashaDyka <beli.rothi2005@gmail.com>
2025-09-14 12:37:12 +0300

    write basic bill list structure

--- src/components/BillList.tsx
+++ src/components/BillList.tsx
@@ -1,2 +1,23 @@
-const BillList = () => <div>Bill List</div>;
+import React from 'react';
+import type { Bill } from '../types/types';
+
+interface BillListProps{
+    bills: Bill[];
+  onSelectBill: (bill: Bill) => void;
+}
+
+const BillList: React.FC<BillListProps> = ({ bills, onSelectBill }) => {
+  return (
+    <div>
+      <h2>Bill List</h2>
+      <ul>
+        {bills.map(bill => (
+          <li key={bill.id} onClick={() => onSelectBill(bill)}>
+            Bill № {bill.id}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default BillList;
