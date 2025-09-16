c488e85730ab9ef41654b00ba4f1856c7ab3152e
SashaDyka <beli.rothi2005@gmail.com>
2025-09-17 01:13:42 +0300

    edit reducers

--- src/features/bills/billsSlice.ts
+++ src/features/bills/billsSlice.ts
@@ -1,53 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
-import type { PayloadAction } from '@reduxjs/toolkit';
-import { v4 as uuidv4 } from 'uuid'; 
-import type { Bill } from '../../types/types';
 
-export interface BillsState {
-  bills: Bill[];
-}
-
-const initialState: BillsState = {
+const initialState = {
   bills: [],
+  selectedBill: null,
 };
 
-
-export const billsSlice = createSlice({
+const billsSlice = createSlice({
   name: 'bills',
   initialState,
   reducers: {
-    addBill: (state, action: PayloadAction<{ billAmount: number; tipPercentage: number }>) => {
-      const { billAmount, tipPercentage } = action.payload;
-      const tipAmount = billAmount * (tipPercentage / 100);
-      const totalAmount = billAmount + tipAmount;
-
-      const newBill: Bill = {
-        id: uuidv4(),
-        billAmount,
-        tipPercentage,
-        totalAmount,
-      };
-      state.bills.push(newBill);
+    setBills: (state, action) => {
+      state.bills = action.payload;
     },
-    removeBill: (state, action: PayloadAction<string>) => {
-      state.bills = state.bills.filter(bill => bill.id !== action.payload);
+    selectBill: (state, action) => {
+      state.selectedBill = action.payload;
     },
-    updateBill: (state, action: PayloadAction<Bill>) => {
-      const { id, billAmount, tipPercentage } = action.payload;
-      const index = state.bills.findIndex(bill => bill.id === id);
+    addBill: (state, action) => {
+      state.bills.push(action.payload);
+    },
+    updateBill: (state, action) => {
+      const index = state.bills.findIndex(bill => bill.id === action.payload.id);
       if (index !== -1) {
-        const tipAmount = billAmount * (tipPercentage / 100);
-        state.bills[index] = {
-          ...state.bills[index],
-          billAmount,
-          tipPercentage,
-          totalAmount: billAmount + tipAmount,
-        };
+        state.bills[index] = action.payload;
       }
+    },
+    deletedBill: (state, action) => {
+      state.bills = state.bills.filter(bill => bill.id !== action.payload);
     },
   },
 });
 
-export const { addBill, removeBill, updateBill } = billsSlice.actions;
-
+export const { setBills, selectBill, addBill, updateBill, deletedBill } = billsSlice.actions;
 export default billsSlice.reducer;
