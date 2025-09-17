3d1fb29b7ae62bc15dfcc60010231789d87baf88
SashaDyka <beli.rothi2005@gmail.com>
2025-09-17 16:21:37 +0300

    add loading

--- src/components/BillList/BillList.tsx
+++ src/components/BillList/BillList.tsx
@@ -7,6 +7,7 @@ import styles from './BillList.module.css';
 interface BillListProps{
     bills: Bill[];
   onSelectBill: (bill: Bill) => void;
+  loading?: boolean;
 }
 
 const BillList: React.FC<BillListProps> = ({ bills, onSelectBill }) => {
