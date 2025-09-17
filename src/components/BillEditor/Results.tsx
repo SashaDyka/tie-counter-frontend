26eadadeeb06e396c2f1976dbcb9cd069c8992bd
SashaDyka <beli.rothi2005@gmail.com>
2025-09-17 21:28:35 +0300

    rename tipPercent

--- src/components/BillEditor/Results.tsx
+++ src/components/BillEditor/Results.tsx
@@ -4,23 +4,23 @@ import styles from './BillEditor.module.css';
 
 interface ResultsProps {
     tipAmount: number; 
-    tipPercent: number;
+    amountPerPerson: number;
     people: Person[];
 }
 
-const Results: React.FC<ResultsProps> = ({ tipAmount, tipPercent, people }) => {
+const Results: React.FC<ResultsProps> = ({ tipAmount, amountPerPerson, people }) => {
 
   return (
     <div className={styles.resultsBox}>
       <h3>Results:</h3>
       <p className={styles.perPerson}>Tip amount: {tipAmount.toFixed(2)}</p>
-      <p className={styles.totalTip}>Total amount: {tipPercent.toFixed(2)}</p>
+      <p className={styles.totalTip}>Total amount: {amountPerPerson.toFixed(2)}</p>
       <p className={styles.perPerson}>How much each person pays: </p>
       
       <ul className={styles.personList}>
         {people.map((person, index) => (
           <li key={index}>
-            {person.name}: {person.tipAmount ? person.tipAmount.toFixed(2) : tipPercent.toFixed(2)}
+            {person.name}: {person.tipAmount ? person.tipAmount.toFixed(2) : amountPerPerson.toFixed(2)}
           </li>
         ))}
       </ul>
