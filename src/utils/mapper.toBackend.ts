b6350eeb600604c4b96b59d99a01281c2099a63f
SashaDyka <beli.rothi2005@gmail.com>
2025-09-18 23:55:49 +0300

    create mappers for req ahd res

---
+++ src/utils/mapper.toBackend.ts
@@ -0,0 +1,34 @@
+import type { BillUI } from './mapper.toFrontend';
+//import { BillRequestDto } from '@/types/BillApi';
+
+
+ export function mapBillToApi(bill: BillUI) {
+  const peopleToAdd = bill.people
+    .filter(p => !p.id || p.id === 0)
+    .map(p => ({
+      name: p.name,
+      individualAmount: p.individualAmount || undefined,    //If the API uses individualAmount (rather than tipAmount), 
+      individualTipPercentage:                              // then that is what should be used, and tipAmount should not be added.
+        p.individualTipPercentage  === bill.tipPercent ? undefined : p.individualAmount,
+    }));
+
+  const peopleToUpdate = bill.people
+    .filter(p => p.id && p.id !== 0)
+    .map(p => ({
+      id: p.id,
+      name: p.name,
+      individualAmount: p.individualAmount || undefined,
+      individualTipPercentage:
+        p.individualTipPercentage  === bill.tipPercent ? undefined : p.individualAmount,
+    }));
+
+  // TODO: добавить peopleToRemove
+
+  return {
+    totalAmount: bill.totalAmount,
+    defaultTipPercentage: bill.tipPercent,
+    peopleToAdd,
+    peopleToUpdate,
+    peopleToRemove: [],
+  };
+}
