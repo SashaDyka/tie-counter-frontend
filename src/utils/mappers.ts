81c39ca4e65d1a6e3a5de7052e3818f27830bae8
SashaDyka <beli.rothi2005@gmail.com>
2025-09-18 21:53:57 +0300

    create bill mapper

---
+++ src/utils/mappers.ts
@@ -0,0 +1,30 @@
+import type { BillResponseDto } from "../types/dto";
+
+export interface BillUI {
+  id: number;
+  totalAmount: number;       
+  defaultTipPercentage: number; 
+  peopleCount: number;    
+  people: {
+    id: number;
+    name: string;
+    individualAmount: number;
+    individualTipPercentage: number;
+  }[];
+}
+
+export const mapBillFromApi = (bill: BillResponseDto): BillUI => {
+  return {
+    id: bill.id,
+    totalAmount: bill.totalAmount,
+    defaultTipPercentage: bill.defaultTipPercentage,
+    peopleCount: bill.people?.length ?? 0,
+    people: bill.people.map(p => ({
+      id: p.id,
+      name: p.name,
+      individualAmount: p.individualAmount,
+      individualTipPercentage: p.individualTipPercentage,
+    }))
+  };
+};
+
